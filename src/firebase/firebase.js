57f3f937f54441bd1588c8cc114418b06ceb9b04
Sasha <khovinalexa@gmail.com>
2020-03-27 12:50:26 -0700

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app'
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
