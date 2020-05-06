e5c25755db40ac16ef3c6b22352e70453fa52e58
Sasha <khovinalexa@gmail.com>
2020-05-06 15:12:46 -0700

    file fic

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,6 +1,9 @@
 const db = require('./util/admin')
 const functions = require('firebase-functions');
 const app = require('express')();
+const cors = require('cors');
+app.use(cors());
+
 const { login, signup } = require('./handlers/users');
 
 app.post('/login', login)
