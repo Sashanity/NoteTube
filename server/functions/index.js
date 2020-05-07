8ccef7f26ac633cc16c5e9b383cfa7acdbb81771
Sasha <khovinalexa@gmail.com>
2020-05-06 18:05:49 -0700

    banch fix

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,12 +1,11 @@
-const db = require('./util/admin')
+const db = require('./util/admin');
 const functions = require('firebase-functions');
 const app = require('express')();
+const { login, signup, auth } = require('./handlers/users');
 const cors = require('cors');
 app.use(cors());
 
-const { login, signup } = require('./handlers/users');
-
-app.post('/login', login)
-app.post('/signup', signup)
+app.post('/login', login);
+app.post('/signup', signup);
 
 exports.api = functions.https.onRequest(app);
