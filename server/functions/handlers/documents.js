fc583c2c29f9d0fedb7a8cc8f8078b9f345ff676
Michael McInerney <mcinerney.michael.t@gmail.com>
2020-08-18 20:11:29 -0700

    Files now upload correctly.

--- server/functions/handlers/documents.js
+++ server/functions/handlers/documents.js
@@ -1,8 +1,9 @@
 const { db, bucket } = require('../util/admin');
 const { ref } = require('firebase-functions/lib/providers/database');
 const fs = require('fs');
-const util = require('util');
-var log_file = fs.createWriteStream('../debug.log', {flags : 'w'});
+const os = require('os');
+const Busboy = require('busboy');
+const path = require('path');
 
 const createKeywords = (input) => {
 	const arrName = [];
@@ -61,23 +62,34 @@ exports.search = (req, res) => {
 };
 
 exports.upload = (req, res) => {
-	try {
-		let file = req.file;
-		;
-		if (file) {
-			log_file.write(new Date()+ ": status: 200 { success }\n")
-			res.status(200).send({
-				status: 'success'
-			});
-		}
-		else{
-			log_file.write(new Date()+ ": status: 400 { failure to get file }\n");
-			res.status(400).send({
-				status: 'failure to get image'
-			});
-		}
-	} catch (error) {
-		log_file.write(new Date()+ ": " + util.format(error) + "\n");
-		console.log("error");
-	}
-};
+	const busboy = new Busboy({ headers: req.headers });
+        // This object will accumulate all the uploaded files, keyed by their name
+        const uploads = {}
+
+        // This callback will be invoked for each file uploaded
+        busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+            console.log(`File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`);
+            // Note that os.tmpdir() is an in-memory file system, so should only 
+            // be used for files small enough to fit in memory.
+            const filepath = path.join(os.tmpdir(), fieldname);
+            uploads[fieldname] = { file: filepath }
+            console.log(`Saving '${fieldname}' to ${filepath}`);
+            file.pipe(fs.createWriteStream(filepath));
+        });
+
+        // This callback will be invoked after all uploaded files are saved.
+        busboy.on('finish', () => {
+            for (const name in uploads) {
+                const upload = uploads[name];
+				const file = upload.file;
+                res.write(`${file}\n`);
+                fs.unlinkSync(file);
+            }
+            res.end();
+        });
+
+        // The raw bytes of the upload will be in req.rawBody.  Send it to busboy, and get
+        // a callback when it's finished.
+        busboy.end(req.rawBody);
+}
+
--- server/functions/index.js
+++ server/functions/index.js
@@ -1,20 +1,13 @@
 const db = require('./util/admin');
 const functions = require('firebase-functions');
 const app = require('express')();
-const multer = require('multer');
+//const Busboy = require('busboy');
 const { login, signup, auth } = require('./handlers/users');
 const { search, upload } = require('./handlers/documents');
-
-const storage = multer({
-    storage: multer.memoryStorage(),
-    limits: {
-      fileSize: 50 * 1024 * 1024 // no larger than 5mb, you can change as needed.
-    }
-  });
 
 app.post('/login', login);
 app.post('/signup', signup);
 app.get('/search', search);
-app.post('/upload', storage.single('file'), upload);
+app.post('/upload', upload);
 
 exports.api = functions.https.onRequest(app);
