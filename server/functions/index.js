b567429d416a0b710f2c439993a20870ec83e8bf
Michael McInerney <mcinerney.michael.t@gmail.com>
2020-08-23 08:46:48 -0700

    Got rid of search references in index

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,11 +2,10 @@ const db = require('./util/admin');
 const functions = require('firebase-functions');
 const app = require('express')();
 const { login, signup, auth } = require('./handlers/users');
-const { search, upload } = require('./handlers/documents');
+const { upload } = require('./handlers/documents');
 
 app.post('/login', login);
 app.post('/signup', signup);
-app.get('/search', search);
 app.post('/upload', upload);
 
 exports.api = functions.https.onRequest(app);
