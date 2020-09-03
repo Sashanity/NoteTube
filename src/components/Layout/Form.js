310cb7ba6d0e41d39839bd6c0381788e47e057b1
Ayesha Iqbal <ayesha.iqbal@sanjoseca.gov>
2020-09-02 19:43:14 -0700

    Upload form Completed

--- src/components/Layout/Form.js
+++ src/components/Layout/Form.js
@@ -1,10 +1,79 @@
 import React from 'react';
 import './modal.css';
 import {useDropzone} from "react-dropzone"
+import { withStyles } from '@material-ui/core/styles';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 
 export const Form = ({ onSubmit }) => {
   const {acceptedFiles, getRootProps, getInputProps} = useDropzone()
 
+  // Toggle Switch CSS
+  const IOSSwitch = withStyles((theme) => ({
+    root: {
+      width: 42,
+      height: 26,
+      padding: 0,
+      margin: theme.spacing(1),
+    },
+    switchBase: {
+      padding: 1,
+      '&$checked': {
+        transform: 'translateX(16px)',
+        color: theme.palette.common.white,
+        '& + $track': {
+          backgroundColor: '#52d869',
+          opacity: 1,
+          border: 'none',
+        },
+      },
+      '&$focusVisible $thumb': {
+        color: '#52d869',
+        border: '6px solid #fff',
+      },
+    },
+    thumb: {
+      width: 24,
+      height: 24,
+    },
+    track: {
+      borderRadius: 26 / 2,
+      border: `1px solid ${theme.palette.grey[400]}`,
+      backgroundColor: theme.palette.grey[50],
+      opacity: 1,
+      transition: theme.transitions.create(['background-color', 'border']),
+    },
+    checked: {},
+    focusVisible: {},
+  }))(({ classes, ...props }) => {
+    return (
+      <Switch
+        focusVisibleClassName={classes.focusVisible}
+        disableRipple
+        classes={{
+          root: classes.root,
+          switchBase: classes.switchBase,
+          thumb: classes.thumb,
+          track: classes.track,
+          checked: classes.checked,
+        }}
+        {...props}
+      />
+    );
+  });
+
+
+//Toggle Switched
+  const [state, setState] = React.useState({
+
+    checkedB: true,
+
+  });
+// Upload dropZone 
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
   const files = acceptedFiles.map((file) => (
     <li key={file.path}>
       {file.path} - {file.size} bytes
@@ -41,6 +110,13 @@ export const Form = ({ onSubmit }) => {
         <p>Click here to Upload Notes</p>
       </div>
     </section>
+    {/* Add toggle switch here */}
+     <FormGroup>
+    <FormControlLabel
+        control={<IOSSwitch checked={state.checkedB} onChange={handleChange} name="checkedB" />}
+        label="Publish Notes"
+      />
+    </FormGroup>
 
       <div className="form-group">
         <button className="form-control btn btn-primary" type="submit">
