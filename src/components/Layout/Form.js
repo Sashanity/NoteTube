43d8a1c8e53f269186d0c8836cfa3655c8a101d2
Ayesha Iqbal <ayesha.iqbal@sanjoseca.gov>
2020-09-14 17:43:39 -0700

    updated toggle switch css

--- src/components/Layout/Form.js
+++ src/components/Layout/Form.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './modal.css';
 import {useDropzone} from "react-dropzone"
+import { green, grey, purple } from '@material-ui/core/colors';
+import FormControl from '@material-ui/core/FormControl';
 import { withStyles } from '@material-ui/core/styles';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -8,72 +10,31 @@ import Switch from '@material-ui/core/Switch';
 
 export const Form = ({ onSubmit }) => {
   const {acceptedFiles, getRootProps, getInputProps} = useDropzone()
+//switched state
 
-  // Toggle Switch CSS
-  const IOSSwitch = withStyles((theme) => ({
-    root: {
-      width: 42,
-      height: 26,
-      padding: 0,
-      margin: theme.spacing(1),
+const PurpleSwitch = withStyles({
+  switchBase: {
+    color: grey[300],
+    '&$checked': {
+      color: green[500],
     },
-    switchBase: {
-      padding: 1,
-      '&$checked': {
-        transform: 'translateX(16px)',
-        color: theme.palette.common.white,
-        '& + $track': {
-          backgroundColor: '#52d869',
-          opacity: 1,
-          border: 'none',
-        },
-      },
-      '&$focusVisible $thumb': {
-        color: '#52d869',
-        border: '6px solid #fff',
-      },
+    '&$checked + $track': {
+      backgroundColor: green[500],
     },
-    thumb: {
-      width: 24,
-      height: 24,
-    },
-    track: {
-      borderRadius: 26 / 2,
-      border: `1px solid ${theme.palette.grey[400]}`,
-      backgroundColor: theme.palette.grey[50],
-      opacity: 1,
-      transition: theme.transitions.create(['background-color', 'border']),
-    },
-    checked: {},
-    focusVisible: {},
-  }))(({ classes, ...props }) => {
-    return (
-      <Switch
-        focusVisibleClassName={classes.focusVisible}
-        disableRipple
-        classes={{
-          root: classes.root,
-          switchBase: classes.switchBase,
-          thumb: classes.thumb,
-          track: classes.track,
-          checked: classes.checked,
-        }}
-        {...props}
-      />
-    );
-  });
+  },
+  checked: {},
+  track: {},
+})(Switch);
+
+const [state, setState] = React.useState({
+  checkedA: true,
+
+});
+const handleChange = (event) => {
+  setState({ ...state, [event.target.name]: event.target.checked });
+};
 
 
-//Toggle Switched
-  const [state, setState] = React.useState({
-
-    checkedB: true,
-
-  });
-// Upload dropZone 
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
   const files = acceptedFiles.map((file) => (
     <li key={file.path}>
       {file.path} - {file.size} bytes
@@ -84,25 +45,23 @@ export const Form = ({ onSubmit }) => {
     <div className="form_section">
     <form className="form" onSubmit={onSubmit}>
       <div className="form_section">
-        <label htmlFor="name">Course Name</label>
+        <label htmlFor="name">Course </label>
         <input className="form-input" id="name" />
       </div>
       <div className="form_section">
-        <label htmlFor="name">Semester </label>
+        <label htmlFor="name"> File Name </label>
         <input className="form-input" id="name" />
       </div>
       <div className="form_section">
         <label htmlFor="name">Instructor Name</label>
         <input className="form-input" id="name" />
       </div>
+
       <div className="form_section">
-        <label htmlFor="name">File Name</label>
+        <label htmlFor="name">Term( Spring/ Fall)</label>
         <input className="form-input" id="name" />
       </div>
-      <div className="form_section">
-        <label htmlFor="name">Course Name</label>
-        <input className="form-input" id="name" />
-      </div>
+      
 
       <section className="dropzone_container">
       <div {...getRootProps({className: "dropzone"})}>
@@ -110,13 +69,15 @@ export const Form = ({ onSubmit }) => {
         <p>Click here to Upload Notes</p>
       </div>
     </section>
-    {/* Add toggle switch here */}
-     <FormGroup>
-    <FormControlLabel
-        control={<IOSSwitch checked={state.checkedB} onChange={handleChange} name="checkedB" />}
+   
+{/* switched for Publish notes  */}
+
+<FormGroup>
+      <FormControlLabel
+        control={<PurpleSwitch checked={state.checkedA} onChange={handleChange} name="checkedA" />}
         label="Publish Notes"
       />
-    </FormGroup>
+</FormGroup>
 
       <div className="form-group">
         <button className="form-control btn btn-primary" type="submit">
--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import SidebarRow from './SidebarRow';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import SystemUpdateAltIcon from '@material-ui/icons/SystemUpdateAlt';
-import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+
 import './sidebar.css';
 import Container from './Container';
 import triggerText from './TriggerButton';
@@ -19,7 +16,7 @@ function Sidebar(){
 return(
 
 <div className="sidebar">
-<h4 className="SidebarRow">Upload Notes</h4>
+<h4 className="SidebarRow"> Useful Links</h4>
 
 <Container triggerText={triggerText} onSubmit={onSubmit} />
 
