1f3499cf33b28371bf6f1a026c960a901c0de8cd
Michael McInerney <mcinerney.michael.t@gmail.com>
2020-09-30 15:46:04 -0700

    Added some comments

--- server/functions/handlers/documents.js
+++ server/functions/handlers/documents.js
@@ -11,11 +11,11 @@ const { response } = require('express');
 exports.upload = (req, res) => {
 
 
-    const busboy = new Busboy({ headers: req.headers });
-    const uploads = {};
-    const idToken = req.query.token;
-    var userID = "";
-    var returnval = {};
+    const busboy = new Busboy({ headers: req.headers }); //Busboy is used to parse the form-data
+    const uploads = {}; //Where the form-data is stored
+    const idToken = req.query.token; //The token of the user
+    var userID = ""; //The user ID that will be received from the user
+    var returnval = {}; //Returns the user's form-data after the upload is complete
 
 
 
@@ -36,21 +36,18 @@ exports.upload = (req, res) => {
 
     //This reads the file fields from the form
     busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-        console.log(`File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`);
         const filepath = path.join(os.tmpdir(), filename); //Gets the temporary directory that the file will go in
         uploads[fieldname] = { file: filepath, name: filename } //Add the file to the uploads array
-        console.log(`Saving '${fieldname}' to ${filepath}`);
         file.pipe(fs.createWriteStream(filepath)); //Takes the file, reads it, then writes it to the temporary dircetory
     });
 
     busboy.on('finish', () => {
         for (const name in uploads) {
-            const upload = uploads[name];
-            const file = upload.file;
-            bucket.upload(file, { destination: `notes/${userID}/${upload.name}` })
+            const upload = uploads[name]; //Get the object with the file
+            const file = upload.file; //Get the file from the object
+            bucket.upload(file, { destination: `notes/${userID}/${upload.name}` }) //Uploads the file to the storage bucket
                 .then(data => {
-                    console.log(data);
-                    const newNote = {
+                    const newNote = { //Create the note object that will be uploaded to Firestore
                         name: returnval.name,
                         filename: upload.name,
                         subject: returnval.subject,
@@ -63,60 +60,51 @@ exports.upload = (req, res) => {
                         timestamp: admin.firestore.Timestamp.fromDate(new Date()),
                     };
 
-                    db.collection('notes').add(newNote).then(function(uploadDocRef){
+                    db.collection('notes').add(newNote).then(function(uploadDocRef){ //
                         
                         fs.unlinkSync(file); //Unlinks and deletes the file
-                        return res.status(200).json({ Status: "Uploaded", noteID: uploadDocRef.id, field: returnval });
+                        return res.status(200).json({ Status: "Uploaded", noteID: uploadDocRef.id, field: returnval }); //Send the successful response back
                     })
                     .catch(function(error){
-                        //fs.unlinkSync(file); //Unlinks and deletes the file
-                        console.log(error);
-                        return res.status(500).json("Error Uploading");
+                        return res.status(500).json("Error Uploading"); //Problem adding the note to the Firestore
                     });
                     
                 }).catch(err => {
                     fs.unlinkSync(file); //Unlinks and deletes the file
-                    console.log('error uploading to storage', err);
-                    return res.status(500).json(err);
+                    return res.status(500).json(err); //Error uploading to storage
                 });
 
         }
         
     });
-    // db.collection('notes').add(newNote)
-    // .then(() => {
-    //     return res.json({ message: 'Note added successfully' });
-    // })
-
 }
 
 exports.preview = (req, res) => {
-    const noteRef = db.collection('notes').doc(req.query.noteid);
-    var fileDir = "";
-    var userID = "";
-    var contentType = "application/xml";
+    const noteRef = db.collection('notes').doc(req.query.noteid); //Get the reference of the note data from Firestore using the note ID from the request
+    var fileDir = ""; //The directory of the file
+    var userID = ""; //The ID of the logged in user
+    var contentType = "application/xml"; //The content type of the response that will be sent back
     if (req.query.token){
-        admin.auth().verifyIdToken(req.query.token).then(function(decodedToken){
+        admin.auth().verifyIdToken(req.query.token).then(function(decodedToken){ //Authenticate the token
             userID = decodedToken.uid; //Get the uid
         }).catch(function (error) {
             return res.status(400).json({Status: "Verification Error"}); //Didn't Log in correctly
         })
     }
-    noteRef.get().then(function(doc){
-        if (doc.exists){
-            noteData = doc.data();
+    noteRef.get().then(function(doc){ //Get the note data from Firestore
+        if (doc.exists){ //If the note data was found (I.E. the note ID was correct)
+            noteData = doc.data(); //Put the data in a variable
             const isPublic = (noteData.public == 'true'); //Converts from string to boolean
-            if (noteData.uploader === userID || isPublic){
-                //const fileDest = downloadFile().catch(console.error);
-                file = bucket.file(`notes/${noteData.uploader}/${noteData.filename}`);
+            if (noteData.uploader === userID || isPublic){ //Verifies that the user is allowed to view the note (either they created it or it is public)
+                file = bucket.file(`notes/${noteData.uploader}/${noteData.filename}`); //Constructs the url from the note data from Firestore
                 
-                fileDir = path.join(os.tmpdir(), noteData.filename);
-                file.createReadStream()
+                fileDir = path.join(os.tmpdir(), noteData.filename); //Get the location of the temporary directory to place the file
+                file.createReadStream() //Read the file
                 .on('error', function(err){
-                    return res.status(500).json({Status: err});
+                    return res.status(500).json({Status: err}); //Error reading the file
                 })
                 .on('response', function(response) {
-                    contentType = response.headers['content-type'];
+                    contentType = response.headers['content-type']; //Get the type of file that it is
                 })
                 .on('end', function() {
                     fs.readFile(fileDir, function (err, data){
@@ -138,7 +126,5 @@ exports.preview = (req, res) => {
         return res.status(500).json({Error: error});
     })
     
-    
-    //return res.status(200).json({ Status: "Found"})
 }
 
