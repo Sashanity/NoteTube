ab454323fc20fde857868326da1643a98fe378c2
Michael McInerney <mcinerney.michael.t@gmail.com>
2020-10-28 15:33:55 -0700

    Added favorite notes to the urls

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,7 +2,7 @@ const db = require('./util/admin');
 const functions = require('firebase-functions');
 const app = require('express')();
 const { login, signup, auth } = require('./handlers/users')
-const { upload, preview, editNote, deleteNote } = require('./handlers/documents');
+const { upload, preview, editNote, deleteNote, favoriteNote } = require('./handlers/documents');
 
 const cors = require('cors');
 app.use(cors());
@@ -13,5 +13,6 @@ app.post('/upload', upload);
 app.get('/preview', preview);
 app.put('/editNote', editNote);
 app.delete('/deleteNote', deleteNote);
+app.put('/favoriteNote', favoriteNote);
 
 exports.api = functions.https.onRequest(app);
