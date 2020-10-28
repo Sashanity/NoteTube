3f97f3e74549f1b24d9390b97734bf9a7c69a26a
Michael McInerney <mcinerney.michael.t@gmail.com>
2020-10-28 15:34:49 -0700

    Changed favorite Note to post

--- server/functions/index.js
+++ server/functions/index.js
@@ -13,6 +13,6 @@ app.post('/upload', upload);
 app.get('/preview', preview);
 app.put('/editNote', editNote);
 app.delete('/deleteNote', deleteNote);
-app.put('/favoriteNote', favoriteNote);
+app.post('/favoriteNote', favoriteNote);
 
 exports.api = functions.https.onRequest(app);
