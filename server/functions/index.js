7c6f2125969b4b9e9511ff195117d831262a402a
Michael McInerney <mcinerney.michael.t@gmail.com>
2020-11-25 14:25:03 -0800

    Added the User Public Notes API

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,7 +2,7 @@ const db = require('./util/admin');
 const functions = require('firebase-functions');
 const app = require('express')();
 const { login, signup, auth, verifyToken } = require('./handlers/users');
-const { upload, preview, userList, editNote, deleteNote } = require('./handlers/documents');
+const { upload, preview, userList, editNote, deleteNote, userPublicNotes } = require('./handlers/documents');
 
 const cors = require('cors');
 app.use(cors());
@@ -13,6 +13,7 @@ app.post('/upload', upload);
 app.get('/preview', preview);
 app.get('/verifyToken', verifyToken);
 app.get('/userList', userList);
+app.get('/userPublicNotes', userPublicNotes);
 app.put('/editNote', editNote);
 app.delete('/deleteNote', deleteNote);
 
